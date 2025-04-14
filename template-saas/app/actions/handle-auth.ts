b7775d769cf586919bd51c85312cee6a61f9ebfa
Bruno Eduardo de Moura Ferreira <brunoemf@gmail.com>
2025-04-14 20:21:56 -0300

    fix: correct formatting and structure in authentication handling

--- template-saas/app/actions/handle-auth.ts
+++ template-saas/app/actions/handle-auth.ts
@@ -4,17 +4,16 @@ import { auth, signIn, signOut } from "@/app/lib/auth"
 
 
 export async function handleAuth() {
-   const session = await auth();
+    const session = await auth();
 
-   if (session) {
+    if (session) {
         // Se o usuário já estiver logado, faça logout
-      return await signOut({
+        return await signOut({
             redirectTo: "/login",
         });
     }
     // Quando o usuário for logado com sucesso ele será redirecionado para o dashboard
-    await signIn("google"), {
+    await signIn("google", {
         redirectTo: "/dashboard",
-    };
-
+    });
 }
--- template-saas/app/lib/auth.ts
+++ template-saas/app/lib/auth.ts
@@ -2,9 +2,17 @@ import { FirestoreAdapter } from "@auth/firebase-adapter"
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { firebaseCert } from "./firebase"
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [Google({
+    clientId: process.env.AUTH_GOOGLE_ID!,
+    clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    authorization: {
+      params: {
+        prompt: "select_account", // força o Google a mostrar a seleção de conta
+      },
+    },
+  })],
   adapter: FirestoreAdapter({
     credential: firebaseCert,
   }),
